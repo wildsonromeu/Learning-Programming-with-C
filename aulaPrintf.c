2b4e3d337bb3079b25d6579c75dd4a8f0f18adca
wildsonromeu <wildsonromeu@gmail.com>
2024-06-06 00:07:43 -0300

    Aula 2.1

---
+++ aulaPrintf.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main() {
+	
+	printf("IMPRIMINDO TEXTO...\n");
+	
+	printf("PULOU A LINHA!\n");
+	
+	printf("100%%\n"); //imprime o caractere %
+	
+	printf("Valor inteiro: %d. \n", 10);
+	
+	printf("Valor em float (f): %.3f \n", 3.151);
+	
+	printf("Imprimindo caractere: %c \n", 'c');
+	
+	printf("Imprimindo palavra: %s \n", "PALAVRA");
+	
+	
+	
+}
---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+int a = 5, b=10;
+
+int soma = a + b;
+
+printf("%d", soma);
+	
+	return 0;
+}
